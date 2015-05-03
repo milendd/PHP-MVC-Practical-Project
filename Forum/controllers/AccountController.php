d13992ed193f5a57883582b519ae0e075b915cdb
milendd <milen_d@abv.bg>
2015-05-03 20:36:25 +0300

    Implement register with validations

--- Forum/controllers/AccountController.php
+++ Forum/controllers/AccountController.php
@@ -8,7 +8,19 @@ class AccountController extends BaseController {
     }
 	
 	public function register() {
-	
+		if ($this->isPost()){
+			$username = $_POST['username'];
+			$password = $_POST['password'];
+			$email = $_POST['email'];
+			
+			if ($this->accountModel->register($username, $password, $email)){
+				$this->addInfoMessage("registration successfull");
+                $this->redirect("home");
+			}
+			else{
+				$this->addErrorMessage("register failed. The username length should be greater than 2");
+			}
+		}
 	}
 	
 	public function login() {
--- Forum/models/AccountModel.php
+++ Forum/models/AccountModel.php
@@ -6,6 +6,25 @@ class AccountModel extends BaseModel {
 	}
 	
 	public function register($username, $password, $email){
+		if (!isset($username) || strlen($username) < 3){
+			return false;
+		}
+	
+		$statement = self::$db->prepare("SELECT COUNT(id) FROM users WHERE username = ?");
+		$statement->bind_param("s", $username);
+		$statement->execute();
+		$result = $statement->get_result()->fetch_assoc();
 		
+		if ($result['COUNT(id)']){
+			return false;
+		}
+		
+		$pass_hash = password_hash($password, PASSWORD_BCRYPT);
+		
+		$registerStatement = self::$db->prepare("INSERT INTO Users (username, pass_hash, email) VALUES (?, ?, ?)");
+		$registerStatement->bind_param("sss", $username, $pass_hash, $email);
+		$registerStatement->execute();
+		
+		return true;
 	}
 }
